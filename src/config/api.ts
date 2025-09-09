54225057ea62d56d2b66e78b2e57d418627ca186
Arshinreji14 <arshinreji12@gmail.com>
2025-09-10 01:19:15 +0530

    checked the hosted link of api web socket and api is working fine

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000'; // Change to your backend URL if needed
+// export const API_BASE_URL = 'http://localhost:5000'; // Change to your backend URL if needed
+
+
+export const API_BASE_URL = 'https://ticketmanagementbackend-1.onrender.com'; // Change to your backend URL if needed
